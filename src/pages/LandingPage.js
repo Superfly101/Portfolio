0ceca32d6ca44a061ecce8f32b9359bff7c1b959
Superfly <danielukoha101@gmail.com>
2022-07-25 19:33:15 +0100

    updating intro

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,10 +3,10 @@ const LandingPage = () => {
     <section className="intro">
       <h1 className="primary">Hi there, my name is</h1>
       <h2 className="big-heading">Daniel Ukoha.</h2>
-      <h3 className="big-heading">I build things for the web.</h3>
+      <h3 className="big-heading">I interactive user interfaces.</h3>
       <p className="description">
-        I am software engineer who's journey of self-learning has brought me to
-        the field of building user interfaced. I am keen on become a mobile
+        I am software engineer and my self-learning journey has brought me to
+        the field of building user interfaces. I am keen on become a mobile
         developer (React-Native), a technical writer, and a community builder.
       </p>
       <p className="description">
