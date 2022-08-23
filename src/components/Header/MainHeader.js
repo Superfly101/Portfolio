8baa3884e7fabf65af75e87bdb6ff802fb525fa1
Superfly <danielukoha101@gmail.com>
2022-08-23 20:48:23 +0100

    making changes to the header

--- src/components/Header/MainHeader.js
+++ src/components/Header/MainHeader.js
@@ -3,32 +3,33 @@ import classes from "./MainHeader.module.css";
 import { useCallback, useEffect, useState } from "react";
 
 const MainHeader = (props) => {
-  const [showHeader, setShowHeader] = useState(true);
-  const [prevScroll, setPrevScroll] = useState(window.scrollY);
-  const [currentScroll, setCurrentScroll] = useState(0);
+  // const [showHeader, setShowHeader] = useState(true);
+  // const [prevScroll, setPrevScroll] = useState(window.scrollY);
+  // const [currentScroll, setCurrentScroll] = useState(0);
 
-  const onScroll = useCallback(() => {
-    setCurrentScroll(window.scrollY);
-    if (prevScroll > currentScroll) {
-      setShowHeader(true);
-    } else {
-      setShowHeader(false);
-    }
-    setPrevScroll(currentScroll);
-  }, [prevScroll, currentScroll]);
+  // const onScroll = useCallback(() => {
+  //   setCurrentScroll(window.scrollY);
+  //   if (prevScroll > currentScroll) {
+  //     setShowHeader(true);
+  //   } else {
+  //     setShowHeader(false);
+  //   }
+  //   setPrevScroll(currentScroll);
+  // }, [prevScroll, currentScroll]);
 
   const [isExpanded, setIsExpanded] = useState(false);
 
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-  }, [onScroll]);
+  // useEffect(() => {
+  //   window.addEventListener("scroll", onScroll);
+  // }, [onScroll]);
 
   const onClickHandler = () => {
     setIsExpanded((prevState) => !isExpanded);
   };
 
   return (
-    <header className={showHeader ? classes.header : classes["hide-header"]}>
+    // <header className={showHeader ? classes.header : classes["hide-header"]}>
+    <header className={classes.header}>
       <div className={classes.logo}>
         <h1>Superfly</h1>
       </div>
